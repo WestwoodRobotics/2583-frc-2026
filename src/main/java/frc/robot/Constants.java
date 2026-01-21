37e876b83e01c373e389e19c7e736ae7e12646d0
Alan <alan.d.morillo@gmail.com>
2026-01-21 17:55:59 -0600

    rebase

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,6 +4,9 @@ import edu.wpi.first.math.geometry.Transform2d;
 import edu.wpi.first.math.geometry.Translation2d;
 
 import com.ctre.phoenix6.CANBus;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.signals.InvertedValue;
+import com.ctre.phoenix6.signals.NeutralModeValue;
 
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
@@ -39,5 +42,56 @@ public class Constants {
         public static final double aimKp = 4.0;
         public static final double aimKi = 0.0;
         public static final double aimKd = 0.1;
+    }
+
+    public static final class IntakeConstants {
+        public static final int kPositionMotorId = 1;
+        public static final int kVelocityMotorId = 2;
+        public static final CANBus kCANBus = new CANBus("rio");
+
+        public static TalonFXConfiguration getPositionMotorConfigs() {
+            TalonFXConfiguration configs = new TalonFXConfiguration();
+            
+            configs.Slot0.kP = 0.0;
+            configs.Slot0.kI = 0.0;
+            configs.Slot0.kD = 0.0;
+            configs.Slot0.kS = 0.0;
+            configs.Slot0.kV = 0.0;
+            configs.Slot0.kA = 0.0;
+            
+            configs.MotionMagic.MotionMagicExpo_kV = 0.0;
+            configs.MotionMagic.MotionMagicExpo_kA = 0.0;
+
+            configs.CurrentLimits.StatorCurrentLimit = 0.0;
+            configs.CurrentLimits.SupplyCurrentLimit = 0.0;
+
+            configs.Feedback.SensorToMechanismRatio = 0.0;
+            
+            configs.MotorOutput.NeutralMode = NeutralModeValue.Brake;
+            configs.MotorOutput.Inverted = InvertedValue.CounterClockwise_Positive;
+            
+            return configs;
+        }
+
+        public static TalonFXConfiguration getVelocityMotorConfigs() {
+            TalonFXConfiguration configs = new TalonFXConfiguration();
+            
+            configs.Slot0.kP = 0.0;
+            configs.Slot0.kI = 0.0;
+            configs.Slot0.kD = 0.0;
+            configs.Slot0.kS = 0.0;
+            configs.Slot0.kV = 0.0;
+            configs.Slot0.kA = 0.0;
+
+            configs.CurrentLimits.StatorCurrentLimit = 0.0;
+            configs.CurrentLimits.SupplyCurrentLimit = 0.0;
+
+            configs.Feedback.SensorToMechanismRatio = 0.0;
+            
+            configs.MotorOutput.NeutralMode = NeutralModeValue.Brake;
+            configs.MotorOutput.Inverted = InvertedValue.CounterClockwise_Positive;
+            
+            return configs;
+        }
     }
 }
---
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -0,0 +1,40 @@
+package frc.robot.subsystems;
+
+import com.ctre.phoenix6.controls.MotionMagicExpoTorqueCurrentFOC;
+import com.ctre.phoenix6.controls.MotionMagicVelocityTorqueCurrentFOC;
+import com.ctre.phoenix6.hardware.TalonFX;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.IntakeConstants;
+
+public class Intake extends SubsystemBase {
+    private final TalonFX m_positionMotor;
+    private final TalonFX m_velocityMotor;
+
+    private final MotionMagicExpoTorqueCurrentFOC m_expoRequest = new MotionMagicExpoTorqueCurrentFOC(0);
+    private final MotionMagicVelocityTorqueCurrentFOC m_velocityRequest = new MotionMagicVelocityTorqueCurrentFOC(0);
+
+    public Intake() {
+        m_positionMotor = new TalonFX(IntakeConstants.kPositionMotorId, IntakeConstants.kCANBus);
+        m_velocityMotor = new TalonFX(IntakeConstants.kVelocityMotorId, IntakeConstants.kCANBus);
+
+        // Apply configurations directly from constants to keep constructor clean of variables
+        m_positionMotor.getConfigurator().apply(IntakeConstants.getPositionMotorConfigs());
+        m_velocityMotor.getConfigurator().apply(IntakeConstants.getVelocityMotorConfigs());
+    }
+
+    /**
+     * Sets the position of the first motor using Motion Magic Expo (Torque Current FOC).
+     * @param position Target position in rotations.
+     */
+    public void setPosition(double position) {
+        m_positionMotor.setControl(m_expoRequest.withPosition(position));
+    }
+
+    /**
+     * Sets the velocity of the second motor using Motion Magic Velocity (Torque Current FOC).
+     * @param velocity Target velocity in rotations per second.
+     */
+    public void setVelocity(double velocity) {
+        m_velocityMotor.setControl(m_velocityRequest.withVelocity(velocity));
+    }
+}
