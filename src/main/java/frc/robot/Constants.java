64dd656565398ec67ced62283588078d801f14cd
alanmorillo5 <alan.d.morillo@gmail.com>
2026-01-21 12:10:42 -0600

    fixed leds, moved to utils

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,5 +1,10 @@
 package frc.robot;
 
 public class Constants {
-    
+
+    public static final class LEDConstants {
+        public static final int candleId = 30;
+        public static final String canBus = "CANivore";
+        public static final int endIndex = 26;
+    }
 }
