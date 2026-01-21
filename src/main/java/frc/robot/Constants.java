520a7810001f067b16b8778b432444fbc4ef85f0
Alan <alan.d.morillo@gmail.com>
2026-01-21 17:55:59 -0600

    quick constants cleanup

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,5 +1,59 @@
 package frc.robot;
 
-public class Constants {
-    
+import com.ctre.phoenix6.CANBus;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.signals.InvertedValue;
+import com.ctre.phoenix6.signals.NeutralModeValue;
+
+public final class Constants {
+    public static final class IntakeConstants {
+        public static final int kPositionMotorId = 1;
+        public static final int kVelocityMotorId = 2;
+        public static final CANBus kCANBus = new CANBus("rio");
+
+        public static TalonFXConfiguration getPositionMotorConfigs() {
+            TalonFXConfiguration configs = new TalonFXConfiguration();
+            
+            configs.Slot0.kP = 0.0;
+            configs.Slot0.kI = 0.0;
+            configs.Slot0.kD = 0.0;
+            configs.Slot0.kS = 0.0;
+            configs.Slot0.kV = 0.0;
+            configs.Slot0.kA = 0.0;
+            
+            configs.MotionMagic.MotionMagicExpo_kV = 0.0;
+            configs.MotionMagic.MotionMagicExpo_kA = 0.0;
+
+            configs.CurrentLimits.StatorCurrentLimit = 0.0;
+            configs.CurrentLimits.SupplyCurrentLimit = 0.0;
+
+            configs.Feedback.SensorToMechanismRatio = 0.0;
+            
+            configs.MotorOutput.NeutralMode = NeutralModeValue.Brake;
+            configs.MotorOutput.Inverted = InvertedValue.CounterClockwise_Positive;
+            
+            return configs;
+        }
+
+        public static TalonFXConfiguration getVelocityMotorConfigs() {
+            TalonFXConfiguration configs = new TalonFXConfiguration();
+            
+            configs.Slot0.kP = 0.0;
+            configs.Slot0.kI = 0.0;
+            configs.Slot0.kD = 0.0;
+            configs.Slot0.kS = 0.0;
+            configs.Slot0.kV = 0.0;
+            configs.Slot0.kA = 0.0;
+
+            configs.CurrentLimits.StatorCurrentLimit = 0.0;
+            configs.CurrentLimits.SupplyCurrentLimit = 0.0;
+
+            configs.Feedback.SensorToMechanismRatio = 0.0;
+            
+            configs.MotorOutput.NeutralMode = NeutralModeValue.Brake;
+            configs.MotorOutput.Inverted = InvertedValue.CounterClockwise_Positive;
+            
+            return configs;
+        }
+    }
 }
