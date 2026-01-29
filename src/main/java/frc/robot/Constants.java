2f175a75be723cffb45422ac93141a27a8bd2b5d
Alan <alan.d.morillo@gmail.com>
2026-01-29 17:52:35 -0600

    added transfer subsystem base

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -7,4 +7,16 @@ public class Constants {
         public static final String canBus = "CANivore";
         public static final int endIndex = 26;
     }
+
+    public static final class TransferConstants {
+        public static final int kFloorId = 1;
+        public static final int kTransferId = 2;
+
+        public static final double kFloorDefaultVel = 0.0;
+        public static final double kFloorIntakeVel = 0.0;
+        public static final double kFloorShootVel = 0.0;
+
+        public static final double kTransferDefaultVel = 0.0;
+        public static final double kTransferShootVel = 0.0;
+    }
 }
