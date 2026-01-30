e3b5e6b8a2da295ec7c91bf043838d796376b6a9
Alan <alan.d.morillo@gmail.com>
2026-01-29 18:20:47 -0600

    finished transfer subsystem

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,5 +1,9 @@
 package frc.robot;
 
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.signals.InvertedValue;
+import com.ctre.phoenix6.signals.NeutralModeValue;
+
 public class Constants {
 
     public static final class LEDConstants {
@@ -11,12 +15,37 @@ public class Constants {
     public static final class TransferConstants {
         public static final int kFloorId = 1;
         public static final int kTransferId = 2;
+        public static final String kCANBus = "CANivore";
 
         public static final double kFloorDefaultVel = 0.0;
         public static final double kFloorIntakeVel = 0.0;
-        public static final double kFloorShootVel = 0.0;
-
         public static final double kTransferDefaultVel = 0.0;
         public static final double kTransferShootVel = 0.0;
+        public static final double kFloorShootVel = 0.0;
+
+        public static TalonFXConfiguration getVelocityMotorConfigs() {
+            TalonFXConfiguration configs = new TalonFXConfiguration();
+            
+            configs.Slot0.kP = 0.0;
+            configs.Slot0.kI = 0.0;
+            configs.Slot0.kD = 0.0;
+            configs.Slot0.kS = 0.0;
+            configs.Slot0.kV = 0.0;
+            configs.Slot0.kA = 0.0;
+
+            configs.CurrentLimits.StatorCurrentLimit = 0.0;
+            configs.CurrentLimits.SupplyCurrentLimit = 0.0;
+
+            configs.MotionMagic.MotionMagicCruiseVelocity = 0.0;
+            configs.MotionMagic.MotionMagicAcceleration = 0.0;
+            configs.MotionMagic.MotionMagicJerk = 0.0;
+
+            configs.Feedback.SensorToMechanismRatio = 0.0;
+            
+            configs.MotorOutput.NeutralMode = NeutralModeValue.Brake;
+            configs.MotorOutput.Inverted = InvertedValue.CounterClockwise_Positive;
+            
+            return configs;
+        }
     }
 }
--- src/main/java/frc/robot/subsystems/Transfer.java
+++ src/main/java/frc/robot/subsystems/Transfer.java
@@ -1,7 +1,41 @@
 package frc.robot.subsystems;
 
+import com.ctre.phoenix6.CANBus;
+import com.ctre.phoenix6.controls.MotionMagicVelocityTorqueCurrentFOC;
+import com.ctre.phoenix6.hardware.TalonFX;
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.TransferConstants;
 
-public class Transfer extends SubsystemBase{
-    
+public class Transfer extends SubsystemBase {
+    private final TalonFX floorMotor = new TalonFX(TransferConstants.kFloorId, new CANBus(TransferConstants.kCANBus));
+    private final TalonFX transferMotor = new TalonFX(TransferConstants.kTransferId, new CANBus(TransferConstants.kCANBus));
+
+    private final MotionMagicVelocityTorqueCurrentFOC floorRequest = new MotionMagicVelocityTorqueCurrentFOC(0);
+    private final MotionMagicVelocityTorqueCurrentFOC transferRequest = new MotionMagicVelocityTorqueCurrentFOC(0);
+
+    public Transfer() {
+        floorMotor.getConfigurator().apply(TransferConstants.getVelocityMotorConfigs());
+        transferMotor.getConfigurator().apply(TransferConstants.getVelocityMotorConfigs());
+    }
+
+    private void runMotors(double floorVel, double transferVel) {
+        floorMotor.setControl(floorRequest.withVelocity(floorVel));
+        transferMotor.setControl(transferRequest.withVelocity(transferVel));
+    }
+
+    public Command defaultCommand() {
+        // Default: Floor spins at default speed, Transfer is stopped (default speed is 0.0 in constants)
+        return run(() -> runMotors(TransferConstants.kFloorDefaultVel, TransferConstants.kTransferDefaultVel));
+    }
+
+    public Command intakeCommand() {
+        // Left Trigger: Floor spins at intake speed, Transfer remains at default (stopped)
+        return run(() -> runMotors(TransferConstants.kFloorIntakeVel, TransferConstants.kTransferDefaultVel));
+    }
+
+    public Command shootCommand() {
+        // Right Trigger: Both motors spin at their respective shooting velocities
+        return run(() -> runMotors(TransferConstants.kFloorShootVel, TransferConstants.kTransferShootVel));
+    }
 }
